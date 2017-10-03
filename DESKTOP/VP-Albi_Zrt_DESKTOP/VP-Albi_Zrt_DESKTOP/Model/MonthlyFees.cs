1496d664492f05f605ef3644937181a4dc8d8c6e
Krisz <bankairyuu@gmail.com>
2017-10-03 21:12:44 +0200

    get, set and constructors for model classes

---
+++ DESKTOP/VP-Albi_Zrt_DESKTOP/VP-Albi_Zrt_DESKTOP/Model/MonthlyFees.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VP_Albi_Zrt_DESKTOP.Model
+{
+    class MonthlyFees
+    {
+        public enum eFeeType { Water, Gas, Electricity, Internet }
+
+        int _ID;
+        DateTime _CreationDate;
+        eFeeType _FeeType;
+        int _Amount;
+
+        public int ID { get => _ID; set => _ID = value; }
+        public DateTime CreationDate { get => _CreationDate; set => _CreationDate = value; }
+        public eFeeType FeeType { get => _FeeType; set => _FeeType = value; }
+        public int Amount { get => _Amount; set => _Amount = value; }
+
+        public MonthlyFees(int ID, DateTime CreationDate, eFeeType FeeType, int Amount)
+        {
+            _ID = ID;
+            _CreationDate = CreationDate;
+            _FeeType = FeeType;
+            _Amount = Amount;
+        }
+    }
+}
--- DESKTOP/VP-Albi_Zrt_DESKTOP/VP-Albi_Zrt_DESKTOP/Model/Task.cs
+++ DESKTOP/VP-Albi_Zrt_DESKTOP/VP-Albi_Zrt_DESKTOP/Model/Task.cs
@@ -8,5 +8,45 @@ namespace VP_Albi_Zrt_DESKTOP.Model
 {
     class Task
     {
+        public enum eStatus { Open, Under_construction, In_work, Done };
+        public enum eAcceptanceProperty { Accepted, Accepted_with_conditions, Denied };
+
+        int _ID;
+        DateTime _CreationDate;
+        User _From;
+        User _To;
+        string _Description;
+        eStatus _TaskStatus;
+        DateTime _RequestedCompletionDate;
+        DateTime _PlannedCompletionDate;
+        eAcceptanceProperty _AcceptanceProperty;
+        string _AcceptanceMessage;
+
+        public int ID { get => _ID; set => _ID = value; }
+        public DateTime CreationDate { get => _CreationDate; set => _CreationDate = value; }
+        public User From { get => _From; set => _From = value; }
+        public User To { get => _To; set => _To = value; }
+        public string Description { get => _Description; set => _Description = value; }
+        public eStatus TaskStatus { get => _TaskStatus; set => _TaskStatus = value; }
+        public DateTime RequestedCompletionDate { get => _RequestedCompletionDate; set => _RequestedCompletionDate = value; }
+        public DateTime PlannedCompletionDate { get => _PlannedCompletionDate; set => _PlannedCompletionDate = value; }
+        public eAcceptanceProperty AcceptanceProperty { get => _AcceptanceProperty; set => _AcceptanceProperty = value; }
+        public string AcceptanceMessage { get => _AcceptanceMessage; set => _AcceptanceMessage = value; }
+
+        public Task(int ID, DateTime CreationDate, User From, User To, string Description, eStatus TaskStatus, DateTime RequestedCompletionDate, DateTime PlannedCompletionDate, eAcceptanceProperty AcceptanceProperty, string AcceptanceMessage)
+        {
+            this.ID = ID;
+            this.CreationDate = CreationDate;
+            this.From = From;
+            this.To = To;
+            this.Description = Description;
+            this.TaskStatus = TaskStatus;
+            this.RequestedCompletionDate = RequestedCompletionDate;
+            this.PlannedCompletionDate = PlannedCompletionDate;
+            this.AcceptanceProperty = AcceptanceProperty;
+            this.AcceptanceMessage = AcceptanceMessage;
+        }
+
+        public Task() { }
     }
 }
