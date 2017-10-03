d89b34b398395f099c99076537d3c81786f74f06
Sipos <uidq5266@contiwan.com>
2017-10-03 16:54:22 +0200

    Main model classes added

---
+++ DESKTOP/VP-Albi_Zrt_DESKTOP/VP-Albi_Zrt_DESKTOP/Model/Expense.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VP_Albi_Zrt_DESKTOP.Model
+{
+    class Expense
+    {
+    }
+}
