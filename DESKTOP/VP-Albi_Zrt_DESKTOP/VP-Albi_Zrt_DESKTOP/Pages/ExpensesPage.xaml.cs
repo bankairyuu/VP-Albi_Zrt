8d2707612d78a0feeef67b91105ca832a050e86c
Sipos <uidq5266@contiwan.com>
2017-10-06 16:56:37 +0200

    demo Expenses

--- DESKTOP/VP-Albi_Zrt_DESKTOP/VP-Albi_Zrt_DESKTOP/Pages/ExpensesPage.xaml.cs
+++ DESKTOP/VP-Albi_Zrt_DESKTOP/VP-Albi_Zrt_DESKTOP/Pages/ExpensesPage.xaml.cs
@@ -22,7 +22,64 @@ namespace VP_Albi_Zrt_DESKTOP.Pages
     {
         public ExpensesPage()
         {
+            List<Model.Expense> DemoExpenses = new List<Model.Expense>();
+
+            Model.User Krisz = new Model.User()
+                {
+                    ID = 2,
+                    Name = "Sipos István Krisztián",
+                    Nickname = "Krisz",
+                    Username = "bankairyuu",
+                    Password = "sárgarigófészek",
+                    CreditCardNumber = "123123123",
+                    Email = "csiga@láb.com",
+                    Phone = "+36309894774"
+                };
+            Model.User Miki = new Model.User()
+                {
+                    ID = 1,
+                    Name = "Herperger Miklós",
+                    Nickname = "Professzor úr",
+                    Username = "hmiki",
+                    Password = "MikiAKirály",
+                    CreditCardNumber = "123123123",
+                    Email = "hmiki@a_nagy.com",
+                    Phone = "+36301234567"
+                };
+
+            DemoExpenses.Add(new Model.Expense()
+            {
+                ID = 1,
+                From = Miki,
+                To = Krisz,
+                Name = "SÖR",
+                Description = "INNI KELL NEM ÉRTED??",
+                SharingType = Model.Expense.eSharingType.Common,
+                Paid = false,
+                ExpenseMonth = DateTime.Now.AddDays(7),
+                CreationTime = DateTime.Now,
+                ApartmentCost = false,
+                Amount = 1200
+            });
+
+            DemoExpenses.Add(new Model.Expense()
+            {
+                ID = 2,
+                From = Krisz,
+                To = Miki,
+                Name = "SÖR",
+                Description = "INNI KELL NEM ÉRTED??",
+                SharingType = Model.Expense.eSharingType.Common,
+                Paid = false,
+                ExpenseMonth = DateTime.Now.AddDays(7),
+                CreationTime = DateTime.Now,
+                ApartmentCost = false,
+                Amount = 1200
+            });
+
             InitializeComponent();
+
+            this.ExpensesDataGrid.ItemsSource = DemoExpenses;
         }
     }
 }
